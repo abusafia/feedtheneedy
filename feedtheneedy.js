05fd47ca65b0085e24084bcb235006b645ae29a3
Shafiq Shafi <abusafia@gmail.com>
2015-09-25 15:22:29 +0100

    Delete Button functional

--- feedtheneedy.js
+++ feedtheneedy.js
@@ -4,7 +4,36 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     persons: function() {
-      return Persons.find({});
+      return Persons.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-person": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var firstname = event.target.firstname.value;
+      var lastname = event.target.lastname.value;
+ 
+      // Insert a person into the collection
+      Persons.insert({
+        firstname: firstname,
+        lastname: lastname,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.firstname.value = "";
+      event.target.lastname.value = "";
+    }
+  });
+
+  Template.person.events({
+    "click .delete": function () {
+      console.log(event);
+      Persons.remove(this._id);
     }
   });
 }
