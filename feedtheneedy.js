5007d5bcfcb8f9dc7627e1a506f774b00496ac1e
Shafiq Shafi <abusafia@gmail.com>
2015-09-25 12:30:47 +0100

    set up a basic list with a basic form

--- feedtheneedy.js
+++ feedtheneedy.js
@@ -1,17 +1,10 @@
+Persons = new Mongo.Collection("persons");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  // This code only runs on the client
+  Template.body.helpers({
+    persons: function() {
+      return Persons.find({});
     }
   });
 }
