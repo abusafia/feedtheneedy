cb0736dc3402c9b1e777ef9d44a1dc8fb4b569cb
Shafiq Shafi <abusafia@gmail.com>
2015-09-28 07:49:40 +0100

    Add Carousel

--- feedtheneedy.js
+++ feedtheneedy.js
@@ -2,6 +2,7 @@ Persons = new Mongo.Collection("persons");
 
 if (Meteor.isClient) {
   // This code only runs on the client
+  Meteor.subscribe("persons");
   Template.body.helpers({
     persons: function() {
       return Persons.find({}, {sort: {createdAt: -1}});
@@ -35,12 +36,9 @@ if (Meteor.isClient) {
 
   Template.person.events({
     "click .delete": function () {
-      console.log(this._id);
       Persons.remove(this._id);
     },
     "submit .edit-person": function (event) {
-      console.log(this._id+" "+this.firstname+" "+this.lastname);
-
       // Get value from form element
       var firstname = event.target.firstname.value;
       var lastname = event.target.lastname.value;
@@ -60,5 +58,8 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+  Meteor.publish("persons", function () {
+    return Persons.find();
   });
 }
